67fc0021476641e7cb88551ac8ffbbaf6f2ccd0c
Roel Magdaleno <roelmagdaleno@gmail.com>
2020-04-25 13:54:42 -0500

    FEATURE: Start counter when scroll using IntersectionObserver

--- assets/js/wp-countup-show-counter.js
+++ assets/js/wp-countup-show-counter.js
@@ -1,5 +1,30 @@
 import { CountUp } from './vendor/countUp.min.js';
 
+const WP_CUPJS_OBSERVER = new IntersectionObserver( WP_CUPJS_startCounterOnScroll, {
+    rootMargin: '100px 0px 100px 0px',
+    threshold: 0.7
+} );
+
+function WP_CUPJS_startCounterOnScroll( entries, observer ) {
+    for ( let i = 0; i < entries.length; i++ ) {
+        const entry = entries[i];
+
+        if ( ! entry.isIntersecting ) {
+            continue;
+        }
+
+        const counterEl = entry.target;
+
+        if ( counterEl.dataset.hasOwnProperty( 'alreadyScrolled' ) ) {
+            continue;
+        }
+
+        counterEl.dataset.alreadyScrolled = 'true';
+
+        WP_CUPJS_startCounter( counterEl );
+    }
+}
+
 function WP_CUPJS_getPrefix( counterId, prefix ) {
     if ( '' === prefix ) {
         return prefix;
@@ -16,8 +41,16 @@ function WP_CUPJS_getSuffix( counterId, suffix ) {
     return `<span class="wp_cup_suffix" id="suffix-${ counterId }">${ suffix }</span>`;
 }
 
-function WP_CUPJS_startCounter( counterEl, endVal, options ) {
-    let countUp = new CountUp( counterEl, parseInt( endVal ), options );
+function WP_CUPJS_startCounter( counterEl ) {
+    const onScroll = !! counterEl.dataset.scroll;
+
+    if ( ! counterEl.dataset.hasOwnProperty( 'alreadyScrolled' ) && onScroll ) {
+        WP_CUPJS_OBSERVER.observe( counterEl );
+        return;
+    }
+
+    let endVal  = WP_CU_JS.endInsideShortcode ? counterEl.innerHTML : counterEl.dataset.end;
+    let countUp = new CountUp( counterEl, parseInt( endVal ), WP_CUPJS_getOptions( counterEl ) );
     const delay = parseInt( counterEl.dataset.delay );
 
     if ( 0 === delay || isNaN( delay ) ) {
@@ -28,28 +61,28 @@ function WP_CUPJS_startCounter( counterEl, endVal, options ) {
     setTimeout( () => countUp.start(), delay );
 }
 
+function WP_CUPJS_getOptions( counterEl ) {
+    return {
+        startVal: counterEl.dataset.start,
+        decimalPlaces: counterEl.dataset.decimals,
+        duration: counterEl.dataset.duration,
+        useGrouping: !! counterEl.dataset.grouping,
+        useEasing: !! counterEl.dataset.easing,
+        separator: counterEl.dataset.separator,
+        decimal: counterEl.dataset.decimal,
+        prefix: WP_CUPJS_getPrefix( counterEl.id, counterEl.dataset.prefix ),
+        suffix: WP_CUPJS_getSuffix( counterEl.id, counterEl.dataset.suffix )
+    };
+}
+
 function WP_CUPJS_initCounters() {
     const counters = document.querySelectorAll( '.counter' );
 
     for ( let i = 0; i < counters.length; i++ ) {
         let counterEl = counters[i];
-        let endVal    = WP_CU_JS.endInsideShortcode ? counterEl.innerHTML : counterEl.dataset.end;
-
         counterEl.setAttribute( 'id', `counter-${ i }` );
 
-        let options = {
-            startVal: counterEl.dataset.start,
-            decimalPlaces: counterEl.dataset.decimals,
-            duration: counterEl.dataset.duration,
-            useGrouping: !! counterEl.dataset.grouping,
-            useEasing: !! counterEl.dataset.easing,
-            separator: counterEl.dataset.separator,
-            decimal: counterEl.dataset.decimal,
-            prefix: WP_CUPJS_getPrefix( counterEl.id, counterEl.dataset.prefix ),
-            suffix: WP_CUPJS_getSuffix( counterEl.id, counterEl.dataset.suffix )
-        };
-
-        WP_CUPJS_startCounter( counterEl, endVal, options );
+        WP_CUPJS_startCounter( counterEl );
     }
 }
 
